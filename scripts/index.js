907920a92a455e31509cdffef6e214840f92e853
Эмиль Чалаев <chalaevemil@yandex.ru>
2023-02-19 23:49:58 +0300

    Проект Место: исправления после первого ревью

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const profileEditButton = document.querySelector(`.profile__edit-button`);
+const popupCloseButton = document.querySelector(`.popup__close`);
+const editPopup = document.querySelector (`.popup`);
+const nameInput = document.querySelector(`.popup__input_el_name`);
+const professionInput = document.querySelector(`.popup__input_el_profession`);
+const profileName = document.querySelector(`.profile__name`);
+const profileProfession = document.querySelector(`.profile__profession`);
+const popupSubmit = document.querySelector(`.popup__form`);
+
+function popupOpened(){
+    editPopup.classList.add(`popup_opened`);
+};
+
+profileEditButton.addEventListener(`click`, popupOpened);
+
+function popupClosed(){
+    editPopup.classList.remove(`popup_opened`);
+};
+
+popupCloseButton.addEventListener('click', popupClosed);
+
+nameInput.value = "Жак-Ив Кусто";
+professionInput.value = "Исследователь океана";
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileProfession.textContent = professionInput.value;
+    popupClosed();
+};
+
+popupSubmit.addEventListener('submit', handleFormSubmit);
